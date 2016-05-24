e4ec99716dc0d93ddea746013ba6ac8100aa0e47
theycallmekevo <kevin2005tran@gmail.com>
2016-05-23 23:03:26 -0700

    z index's

---
+++ Portfolio_Page/feelingSafari/index2JS.js
@@ -0,0 +1,14 @@
+
+$(function(){
+
+  $('input[name=submit]').on('click',function(){
+    $('.puzzlePiece1').css('visibility', 'visible')
+  });
+});
+
+// $(function(){
+//
+//   $('input[name=firstName]').on('click',function(){
+//     $('.Chris').addClass('animated bounceInRight').css('visibility', 'visible')
+//   });
+// });

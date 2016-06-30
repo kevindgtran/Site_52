cd2619ddb5d8bd376c23972b712d1319b1867d4d
theycallmekevo <kevin2005tran@gmail.com>
2016-06-29 23:04:40 -0700

    listeners

--- Portfolio_Page/AzatMardan/knock-knock.js
+++ Portfolio_Page/AzatMardan/knock-knock.js
@@ -18,3 +18,18 @@ emitter.emit('knock')
 var addon = require('./build/Release/addon');
 
 console.log(addon.hello()); // world
+
+//////////////////////////////////////////
+
+// weekly.js
+var Job = require('./job.js')
+var job = new Job()
+
+job.on('done', function(details){
+  console.log('Weekly email job was completed at', details.completedOn)
+  // job.removeAllListeners()
+
+})
+
+job.process()
+// job.emit('start')

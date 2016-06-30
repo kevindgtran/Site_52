2d0af1965a80e98a73f5bdc51698014d3a9733ac
theycallmekevo <kevin2005tran@gmail.com>
2016-06-29 23:29:19 -0700

    console.log

--- Portfolio_Page/AzatMardan/knock-knock.js
+++ Portfolio_Page/AzatMardan/knock-knock.js
@@ -183,3 +183,12 @@ new WebpackDevServer(webpack(config), {
 
   console.log('Listening at http://localhost:3000/')
 })
+
+////////////////////////////////////////////////////////
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');

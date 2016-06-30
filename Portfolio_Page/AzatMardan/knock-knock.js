3137ab7b5b1ebb9eed559c2ab117fa365e6dae2e
theycallmekevo <kevin2005tran@gmail.com>
2016-06-29 23:23:42 -0700

    web pack!

--- Portfolio_Page/AzatMardan/knock-knock.js
+++ Portfolio_Page/AzatMardan/knock-knock.js
@@ -116,3 +116,70 @@ app.get('/home', function(req, res) {
   res.end('Bonjour, monde !')
 })
 app.listen(process.argv[2])
+
+///////////////////////////////////////////////////////
+
+var path = require('path')
+var webpack = require('webpack')
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // 'only' prevents reload on syntax errors
+    './source/app.jsx',
+  ],
+  output: {
+    path: __dirname + '/public/js',
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  devtool: 'cheap-eval-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|public)/,
+        loaders: [
+          'react-hot',
+          'babel'
+        ]
+      },
+      { test: /\.json$/, loader: 'json-loader'},
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ],
+    noParse: /node_modules\/json-schema\/lib\/validate\.js/
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    console: false,
+    'coffee-script': 'mock'
+  },
+  amd: { jQuery: true },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+}
+
+//////////////////////////////////////////////////
+
+var webpack = require('webpack')
+var WebpackDevServer = require('webpack-dev-server')
+var config = require('./webpack.config')
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  contentBase: 'public',
+  inline: true,
+  stats: { colors: true }
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('Listening at http://localhost:3000/')
+})

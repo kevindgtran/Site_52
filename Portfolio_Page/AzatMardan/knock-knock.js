f879104fd8e6f01772182afd64b217be5b35dda2
theycallmekevo <kevin2005tran@gmail.com>
2016-06-29 23:08:58 -0700

    app.listener

--- Portfolio_Page/AzatMardan/knock-knock.js
+++ Portfolio_Page/AzatMardan/knock-knock.js
@@ -107,3 +107,12 @@ function query (mode) {
 }
 
 module.exports = exercise
+
+//////////////////////////////////////////////////////
+
+var express = require('express')
+var app = express()
+app.get('/home', function(req, res) {
+  res.end('Bonjour, monde !')
+})
+app.listen(process.argv[2])

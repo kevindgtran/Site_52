069068b82c53ba31089006511a7dec0c8191bd26
theycallmekevo <kevin2005tran@gmail.com>
2016-06-29 23:37:26 -0700

    nodeJS

--- Portfolio_Page/AzatMardan/knock-knock.js
+++ Portfolio_Page/AzatMardan/knock-knock.js
@@ -223,3 +223,25 @@ app.all('*', function(req, res) {
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+////////////////////////////////////////////////////////
+
+var assert = require('assert');
+var expected, current;
+before(function(){
+  expected = ['a', 'b', 'c'];
+})
+describe('String#split', function(){
+  beforeEach(function(){
+    current = 'a,b,c'.split(',');
+  })
+  it('should return an array', function(){
+    assert(Array.isArray(current));
+  });
+  it('should return the same array', function(){
+    assert.equal(expected.length, current.length, 'arrays have equal length');
+    for (var i=0; i<expected.length; i++) {
+      assert.equal(expected[i], current[i], i + 'element is equal');
+    }
+  })
+})
